20cfa92b20ec04c62004e1714e5d3c701695b012
Satwinder Singh <satwinder4955@gmail.com>
2024-03-27 15:45:30 -0400

    button
    In this we learn to update name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import {useState}  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import User from './User';
 
 function App() {
+  const [users, setUsers] = useState("Sam");
+  function updateData(){
+    return(
+      setUsers("Smith")
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Hello world</h1>
+      <h1>{users}</h1>
+      <button onClick={updateData}>Update User</button>
+      <User />
       </header>
     </div>
   );
---
+++ src/User.js
@@ -0,0 +1,23 @@
+
+
+//////////// Class Component
+// import React from 'react'
+// class User extends  React.Component{
+//     render(){
+//         return(
+//             <h1>This is a class Component</h1>
+//         )
+//     }
+// }
+// export default User;
+
+//////////// functional Component
+
+function User(){
+    //JSX => using html syntax  in javascript
+    return( 
+        <h1> This is a function Component and we gonna use this one most of the time</h1>
+    );
+}
+
+export default User;
